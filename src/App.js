fe547a4460f4c6c8d67c55561cdf1a4f9fa5e768
Matt <mpaverill@gmail.com>
2021-01-19 15:19:03 -0500

    Added Rank and particles background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number:{
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render(){
     return (
       <div className="App">
+        <Particles className='particles' 
+          params={particlesOptions}
+        />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
                {/*<FaceRecognition />*/}
       </div>
